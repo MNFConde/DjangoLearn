935d90b95ca9c9d1ed1a73ee27997847dae90710
MNF_Condé <catyrenparadox@outlook.com>
2026-01-25 21:19:25 +0800

    初步搭建投票功能

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -3,19 +3,23 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
 from django.shortcuts import (
     render,
-    get_object_or_404,    
+    get_object_or_404,
 )
 from django.db.models import F
 from django.urls import reverse
 
+
 def index(request):
     lastest_questions = Question.objects.order_by("-pub_date")[:5]
     # print(lastest_questions)
-    index_template = loader.get_template('polls/index.html')
-    return HttpResponse(index_template.render(
-        {'latest_question_list': lastest_questions},
-        request,
-    ))
+    index_template = loader.get_template("polls/index.html")
+    return HttpResponse(
+        index_template.render(
+            {"latest_question_list": lastest_questions},
+            request,
+        )
+    )
+
 
 def index_second(request):
     lastest_question = Question.objects.order_by("-pub_date")[:5]
@@ -23,23 +27,33 @@ def index_second(request):
         request,
         "polls/index.html",
         {
-            'latest_question_list': lastest_question,
+            "latest_question_list": lastest_question,
         },
     )
+
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(
         request,
-        'polls/detail.html',
+        "polls/detail.html",
         {
-            'question': question,
+            "question": question,
         },
     )
 
+
 def results(request, question_id):
-    return HttpResponse(f"正在查看问题结果 {question_id}")
-    
+    question = get_object_or_404(Question, pk=question_id)
+    return render(
+        request,
+        "polls/results.html",
+        {
+            "question": question,
+        },
+    )
+
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
