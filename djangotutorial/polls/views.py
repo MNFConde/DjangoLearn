a53419fc97515d859a5fbc4dcda61bfa2c8b5387
MNF_Condé <catyrenparadox@outlook.com>
2026-01-25 13:36:03 +0800

    Writing your first Django app, part 3

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,4 +1,44 @@
+from .models import Question
 from django.http import HttpResponse
+from django.template import loader
+from django.http import Http404
+from django.shortcuts import (
+    render,
+    get_object_or_404,    
+)
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    lastest_questions = Question.objects.order_by("-pub_date")[:5]
+    # print(lastest_questions)
+    index_template = loader.get_template('polls/index.html')
+    return HttpResponse(index_template.render(
+        {'latest_question_list': lastest_questions},
+        request,
+    ))
+
+def index_second(request):
+    lastest_question = Question.objects.order_by("-pub_date")[:5]
+    return render(
+        request,
+        "polls/index.html",
+        {
+            'latest_question_list': lastest_question,
+        },
+    )
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(
+        request,
+        'polls/detail.html',
+        {
+            'question': question,
+        },
+    )
+
+def results(request, question_id):
+    return HttpResponse(f"正在查看问题结果 {question_id}")
+    
+def vote(request, question_id):
+    return HttpResponse(f"给问题 {question_id} 投票")
+
---
+++ django笔记.md
@@ -0,0 +1,5 @@
+
+## uv 相关
+1. uv add Django 来保证环境中有 django 可以使用
+2. uv run django-admin startproject projectname 来创建环境
+
